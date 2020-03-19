48167aa9a8b902a70d8de490f4e0be732ca34d07
F1rman <ihorkharchyshyn@gmail.com>
2020-03-19 18:30:28 +0200

    dsds

--- js/main.js
+++
@@ -1,7 +0,0 @@
-(function(e){
-    jQuery(document).textmistake({
-        'mailTo': 'ihorkharchyshyn(d0g)gmail.com'.replace(/\(d0g\)/, '@'),
-        'mailFrom': 'textmistake(d0g)yourdomain.com'.replace(/\(d0g\)/, '@'),
-        'mandrillKey': '4f41aaebcabc974360f74b9ff5999efb-us19', // Get your - https://mandrill.com/signup/
-    });
-})(jQuery)
