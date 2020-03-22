ab1750e771ab2bdcfe622853bcb1f3a624a7fae8
F1rman <ihorkharchyshyn@gmail.com>
2020-03-22 05:20:17 +0200

    asd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+
+
+
 var app = angular.module("Artmoney", []);
 app.controller("Global", function($scope) {
   $scope.products = [{
@@ -108,22 +112,28 @@ console.log(quantity, count);
 
     var firebaseConfig = {
       apiKey: "AIzaSyANByuaeDUXVVc9FxXt522AvKj-sbytUCA",
-      authDomain: "dostavymo-b160d.firebaseapp.com",
-      databaseURL: "https://dostavymo-b160d.firebaseio.com",
-      projectId: "dostavymo-b160d",
-      storageBucket: "dostavymo-b160d.appspot.com",
-      messagingSenderId: "390587806668",
-      appId: "1:390587806668:web:5ab15e74588b1ad65f2369",
-      measurementId: "G-GJ7TCLH5QR"
+        authDomain: "dostavymo-b160d.firebaseapp.com",
+        databaseURL: "https://dostavymo-b160d.firebaseio.com",
+        projectId: "dostavymo-b160d",
+        storageBucket: "dostavymo-b160d.appspot.com",
+        messagingSenderId: "390587806668",
+        appId: "1:390587806668:web:5ab15e74588b1ad65f2369",
+        measurementId: "G-GJ7TCLH5QR"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
+      firebase.analytics();
 
-
-    var messagesRef = firebase.database().ref('message');
-    console.log(messagesRef);
-
+    var database = firebase.database();
+        console.log(database);
+        function writeUserData() {
+          firebase.database().set({
+            username: 'name',
+            email: 'email',
+            profile_picture : 'imageUrl'
+          });
+        }
+    writeUserData()
 
   })
 });
